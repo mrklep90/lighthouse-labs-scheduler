faf35b7eea61bf247cd870a521b6b8e803c7972b
Michael Klepac <mklepac90@gmail.com>
2020-09-29 17:57:12 -0400

    added Show component

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 storiesOf("Button", module)
   .addParameters({
@@ -148,5 +149,13 @@ storiesOf("InterviewerListItem", module)
     .add("Empty", () => (
       <Empty
         onAdd={action("onAdd")}
+      />
+    ))
+    .add("Show", () => (
+      <Show
+        student="Lydia Miller-Jones"
+        interviewer={interviewer}
+        onEdit={action("onEdit")}
+        onDelete={action("onDelete")}
       />
     ));
