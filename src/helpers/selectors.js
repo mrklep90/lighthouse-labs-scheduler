315ea8235767396a3f0abaa18e2dc0166072760d
Michael Klepac <mklepac90@gmail.com>
2020-09-30 19:26:53 -0400

    added selector function to retrieve appointments for specific day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,29 @@
+export function getAppointmentsForDay(state, day) {
+
+  if (state.days.length === 0) {
+    return [];
+  }
+
+  let requestedDay;
+  for (const d of state.days) {
+    if (d.name === day) {
+      requestedDay = d;
+    }
+  }
+
+  if (requestedDay === undefined) {
+    return [];
+  }
+
+  const dayAppointments = requestedDay.appointments;
+  let appointmentDetails = [];
+
+  for (const appointment in state.appointments) {
+    if (dayAppointments.includes(state.appointments[appointment].id)) {
+      appointmentDetails.push(state.appointments[appointment]);
+    }
+  }
+
+  return appointmentDetails;
+
+}
