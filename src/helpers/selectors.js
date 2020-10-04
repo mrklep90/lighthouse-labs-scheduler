7b89922293a9dc8d51cd0f18071a1cba2426e68d
Michael Klepac <mklepac90@gmail.com>
2020-10-04 18:40:00 -0400

    refactored selectors

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,62 +1,71 @@
-//OMG THIS IS TERRIBLE - MUST REFACTOR!!!!!!
+// OMG THIS IS TERRIBLE - MUST REFACTOR!!!!!!
+// TERNARY OPERATORS??? LETS TRY IT!!!
 
 export function getAppointmentsForDay(state, day) {
 
-  let requestedDay = (state.days.filter(d => d.name === day))[0];
+  // let requestedDay = (state.days.filter(d => d.name === day))[0];
+  const requestedDay = state.days.find(d => d.name === day);
+
   
-  if (state.days.length === 0 || requestedDay === undefined) {
-  return [];
-  }
+  // if (state.days.length === 0 || requestedDay === undefined) {
+  // return [];
+  // }
 
-  const dayAppointments = requestedDay.appointments;
-  let appointmentDetails = [];
+  // const dayAppointments = requestedDay.appointments;
+  // let appointmentDetails = [];
 
-  for (const appointment in state.appointments) {
-    if (dayAppointments.includes(state.appointments[appointment].id)) {
-      appointmentDetails.push(state.appointments[appointment]);
-    }
-  }
+  // for (const appointment in state.appointments) {
+  //   if (dayAppointments.includes(state.appointments[appointment].id)) {
+  //     appointmentDetails.push(state.appointments[appointment]);
+  //   }
+  // }
 
-  return appointmentDetails;
+  // return appointmentDetails;
+  return requestedDay ? requestedDay.appointments.map(key => state.appointments[key]) : [];
 
 }
 
 export function getInterviewersForDay(state, day) {
 
-  let requestedDay = (state.days.filter(d => d.name === day))[0];
+  // let requestedDay = (state.days.filter(d => d.name === day))[0];
+  const requestedDay = state.days.find(d => d.name === day);
 
-  if (state.days.length === 0 || requestedDay === undefined) {
-    return [];
-  }
 
-  const dayInterviewers = requestedDay.interviewers;
-  let interviewerDetails = [];
+  // if (state.days.length === 0 || requestedDay === undefined) {
+  //   return [];
+  // }
 
-  for (const interviewer in state.interviewers) {
-    if (dayInterviewers.includes(state.interviewers[interviewer].id)) {
-      interviewerDetails.push(state.interviewers[interviewer]);
-    }
-  }
+  // const dayInterviewers = requestedDay.interviewers;
+  // let interviewerDetails = [];
 
-  return interviewerDetails;
+  // for (const interviewer in state.interviewers) {
+  //   if (dayInterviewers.includes(state.interviewers[interviewer].id)) {
+  //     interviewerDetails.push(state.interviewers[interviewer]);
+  //   }
+  // }
+
+  // return interviewerDetails;
+  return requestedDay ? requestedDay.interviewers.map(key => state.interviewers[key]) : [];
 
 }
 
 export function getInterview(state, interview) {
 
-  if (!interview) {
-    return null;
-  }
+  // if (!interview) {
+  //   return null;
+  // }
 
-  const interviewerID = interview.interviewer;
+  // const interviewerID = interview.interviewer;
 
-  const interviewDetails = {
+  // const interviewDetails = {
 
-    "student": interview.student,
-    "interviewer": state.interviewers[interviewerID]
+  //   "student": interview.student,
+  //   "interviewer": state.interviewers[interviewerID]
 
-  }
+  // }
 
-  return interviewDetails;
+  // return interviewDetails;
+  
+  return interview ? {...interview, interviewer: state.interviewers[interview.interviewer]} : null;
 
 }
